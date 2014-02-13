ffc2189c67469ea06acd707f4b45f99b9704d533
Rayland <dev.rayje@gmail.com>
2014-02-13 13:31:46 -0700

    Update server.c

--- server.c
+++ server.c
@@ -1,25 +1,25 @@
 #include "util.h"
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
+    if (argc != 2) {
         die("Parameters: <Server Port>");
     }
 
     int sock = setupServer(argv[1]);
 
     for (;;) {
-	    struct sockaddr_in clntAddr;
-	    socklen_t clntAddrLen = sizeof(clntAddr);
+        struct sockaddr_in clntAddr;
+        socklen_t clntAddrLen = sizeof(clntAddr);
 
-	    int clntSock = accept(sock, (struct sockaddr *) &clntAddr, &clntAddrLen);
-	    if (clntSock < 0) {
-	        die("accept() failed");
-	    }
+        int clntSock = accept(sock, (struct sockaddr *) &clntAddr, &clntAddrLen);
+        if (clntSock < 0) {
+            die("accept() failed");
+        }
 
-	    char buffer[BUFSIZE];
-	    _recv(clntSock, buffer);
-	    _send(clntSock, "1");
-	}
+        char buffer[BUFSIZE];
+        _recv(clntSock, buffer);
+        _send(clntSock, "1");
+    }
 
     return 0;
 }
